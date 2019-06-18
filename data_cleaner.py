8d1c27af69a03f89b902c3945a76b60957b9a51f
Martín Stancanelli <mastanca14@gmail.com>
2019-06-17 22:22:22 -0300

    Add script to clean raw data, header row left

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,14 +6,15 @@ Y_CSV_PATH = "./data/y_train.csv"
 
 
 def main():
-    raw_data = pd.read_csv(CSV_PATH)
-    raw_data.drop(raw_data.index[0])
-    raw_data.drop(['ID_code', 'target'], axis=1)
-    raw_data.to_csv(X_CSV_PATH)
+    raw_data = pd.read_csv(CSV_PATH,)
+    raw_data = raw_data.drop(['ID_code', 'target'], axis=1)
+    raw_data.to_csv(X_CSV_PATH, index=None)
 
     raw_data = pd.read_csv(CSV_PATH)
-    raw_data[['target']]
-    raw_data.to_csv(Y_CSV_PATH)
+    raw_data = raw_data[['target']]
+    raw_data.to_csv(Y_CSV_PATH, index=None)
+
+    return 0
 
 
 if __name__ == "__main__":
